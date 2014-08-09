813eb7920a488b9acda124a6e9afee0cce39dcc4
Alberto Jauregui <bastertwo@gmail.com>
2014-08-09 23:35:50 +0000

    Add books

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,17 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+Book.destroy_all
+
+Book.create!([{
+		title: 'Hyperion',
+		author: 'Dan Simmons',
+		description: "Probably my favorite science fiction book (and series) I've ever read.",
+		amazon_id: '0553283685',
+		rating: 5,
+		finished_on: 10.days.ago
+	}, {
+		title: "Jony Ive: The genius behind Apple's Greatest products",
+		author: "Leander Kahney",
+		description: "Even though I respect Ive, I felt this biography only hit the skin deep.",
+		amazon_id: "159184617X",
+		rating: 4,
+		finished_on: 1.day.ago
+		}]);
